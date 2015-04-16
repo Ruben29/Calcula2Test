53024b4a4795af826b2c64ee55436181918dae7e
rfernandezgonzalez <rfernandezgonzalez@atanasoft21.danielcastelao.org>
2015-04-16 09:58:04 +0200

    Clase Vista terminada!

--- src/calcula2/Vista.java
+++ src/calcula2/Vista.java
@@ -1,15 +1,17 @@
 package calcula2;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author rfernandezgonzalez
  */
 public class Vista {
 
-    public static void Imprimir() {
+    public static void Imprimir(float resultado) {
+               
+        JOptionPane.showMessageDialog(null, "El resultado de la operaicón es: " + resultado);
         
-        System.out.println("El resultado es:");
-
     }
 
 }
