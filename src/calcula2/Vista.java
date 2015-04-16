a54703abb80aa3dcdadef8d0d6700e7b5542c664
rfernandezgonzalez <rfernandezgonzalez@atanasoft21.danielcastelao.org>
2015-04-16 09:39:53 +0200

    Terminados primeros avances!

---
+++ src/calcula2/Vista.java
@@ -0,0 +1,15 @@
+package calcula2;
+
+/**
+ *
+ * @author rfernandezgonzalez
+ */
+public class Vista {
+
+    public static void Imprimir() {
+        
+        System.out.println("El resultado es:");
+
+    }
+
+}
