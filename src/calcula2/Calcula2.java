cdfb522707ed8fc8f87d95afacaccfad1326a87c
rfernandezgonzalez <rfernandezgonzalez@atanasoft21.danielcastelao.org>
2015-04-16 09:33:34 +0200

    Commit inicial!

---
+++ src/calcula2/Calcula2.java
@@ -0,0 +1,22 @@
+package calcula2;
+
+/**
+ *
+ * @author rfernandezgonzalez
+ */
+public class Calcula2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        
+
+    }
+    
+    public float realizarOperacion(){
+        
+    }
+
+}
