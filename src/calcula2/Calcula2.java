4d4047abe69e7bb777326a96e2f30505f3ccddd4
rfernandezgonzalez <rfernandezgonzalez@atanasoft21.danielcastelao.org>
2015-04-20 09:44:17 +0200

    Proyecto terminado!

--- src/calcula2/Calcula2.java
+++ src/calcula2/Calcula2.java
@@ -13,83 +13,24 @@ public class Calcula2 {
      */
     public static void main(String[] args) {
 
-        Modelo contenedor = new Modelo();
-
-        float n1 = Float.parseFloat(JOptionPane.showInputDialog("Número 1"));
-        contenedor.setNum1(n1);
-
-        float n2 = Float.parseFloat(JOptionPane.showInputDialog("Número 2"));
-        contenedor.setNum2(n2);
-
-        String caso = JOptionPane.showInputDialog("Introduzca lo que desea hacer:"
-                + "\n 1- Sumar."
-                + "\n 2- Restar."
-                + "\n 3- Multiplicar."
-                + "\n 4- Dividir"
-                + "\n 5- Salir");
-
-        contenedor.setOperacion(op);
-
-        float res = realizaOperacion(contenedor.getNum1(), contenedor.getNum2(), contenedor.getOperacion());
-        contenedor.setResultado(res);
-
-        Vista.imprimir(contenedor);
-
     }
 
-    public float realizarOperacion(Modelo contenedor) {
+    public float realizaOperacion(Modelo contenedor) {
 
         float resultado = 0;
 
-        Modelo cal = new Modelo();
-        Vista resu = new Vista();
+        if (contenedor.operacion.equals("suma")) {
 
-        cal.setNum1(29);
-        cal.setNum2(21);
-        cal.setOperacion("Resta");
+            contenedor.resultado = contenedor.num1 + contenedor.num2;
 
-        do {
+        } else if (contenedor.operacion.equals("resta")) {
 
-            switch (Integer.parseInt(caso)) {
+        } else if (contenedor.operacion.equals("multiplicacion")) {
 
-                case 1:
-                    cal.setNum1(29);
-                    break;
+        } else if (contenedor.operacion.equals("division")) {
 
-                case 2:
-                    obj1.darBaixa();
-                    break;
-                case 3:
-                    obj1.buscarAlumno();
-                    break;
-                case 4:
-                    aux = true;
-                    break;
+        }
 
-                default:
-                    JOptionPane.showMessageDialog(null, "Introduzca una opcion valida");
-                    break;
-            }
-
-        } while (aux == false);
-
-        /*if (contenedor.operacion.equals("Suma")) {
-
-         contenedor.resultado = contenedor.num1 + contenedor.num2;
-
-         } else if (contenedor.operacion.equals("Resta")) {
-
-         contenedor.resultado = contenedor.num1 - contenedor.num2;
-
-         } else if (contenedor.operacion.equals("Multiplicación")) {
-
-         contenedor.resultado = contenedor.num1 * contenedor.num2;
-
-         } else if (contenedor.operacion.equals("División")) {
-
-         contenedor.resultado = contenedor.num1 / contenedor.num2;
-
-         }*/
         return resultado;
 
     }
