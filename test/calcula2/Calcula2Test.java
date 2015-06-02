96c09afd1648f19838a2f3bfde0e838da058026d
rfernandezgonzalez <rfernandezgonzalez@atanasoft21.danielcastelao.org>
2015-06-02 08:57:08 +0200

    Test Lógicos realizado!

--- test/calcula2/Calcula2Test.java
+++ test/calcula2/Calcula2Test.java
@@ -17,22 +17,22 @@ import static org.junit.Assert.*;
  * @author rfernandezgonzalez
  */
 public class Calcula2Test {
-    
+
     public Calcula2Test() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
@@ -40,61 +40,61 @@ public class Calcula2Test {
     /**
      * Test of main method, of class Calcula2.
      */
-    
-
     /**
      * Test of realizarOperacion method, of class Calcula2.
      */
     @Test
     public void testRealizarOperacionSuma() {
-        System.out.println("realizarOperacion");
-        float num1 = 1.0F;
-        float num2 = 2.0F;
-        int op = 1;
-        float expResult = 3.0F;
-        float result = Calcula2.realizarOperacion(num1, num2, op);
+
+        System.out.println("realizaOperacion");
+        Modelo mod = new Modelo(5, 5, 1, 10);
+        Controlador instance = new Controlador();
+        float expResult = 10.0F;
+        float result = instance.realizaOperacion(mod);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
 
     }
-    
+
     @Test
     public void testRealizarOperacionResta() {
-        System.out.println("realizarOperacion");
-        float num1 = 9.0F;
-        float num2 = 10.0F;
-        int op = 2;
-        float expResult = -1.0F;
-        float result = Calcula2.realizarOperacion(num1, num2, op);
+
+        System.out.println("realizaOperacion");
+        Modelo mod = new Modelo(5, 5, 1, 10);
+        Controlador instance = new Controlador();
+        float expResult = 10.0F;
+        float result = instance.realizaOperacion(mod);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
 
     }
+
     //Este método da error de forma intencionada
+
     @Test
     public void testRealizarOperacionMultiplicar() {
-        System.out.println("realizarOperacion");
-        float num1 = 5.0F;
-        float num2 = 0.0F;
-        int op = 3;
-        float expResult = 1.0F;
-        float result = Calcula2.realizarOperacion(num1, num2, op);
+
+        System.out.println("realizaOperacion");
+        Modelo mod = new Modelo(5, 5, 1, 10);
+        Controlador instance = new Controlador();
+        float expResult = 10.0F;
+        float result = instance.realizaOperacion(mod);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
 
     }
-    
+
     @Test
     public void testRealizarOperacionDividir() {
-        System.out.println("realizarOperacion");
-        float num1 = 3.0F;
-        float num2 = 2.0F;
-        int op = 4;
-        float expResult = 1.5F;
-        float result = Calcula2.realizarOperacion(num1, num2, op);
+
+        System.out.println("realizaOperacion");
+        Modelo mod = new Modelo(5, 5, 1, 10);
+        Controlador instance = new Controlador();
+        float expResult = 10.0F;
+        float result = instance.realizaOperacion(mod);
         assertEquals(expResult, result, 0.0);
         // TODO review the generated test code and remove the default call to fail.
         //fail("The test case is a prototype.");
